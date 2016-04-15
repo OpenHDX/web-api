50bd97314aaaad51b2f80d77a2f9586a02a311a3
Fong-Wan Chau <work@fongwanchau.com>
2016-04-14 21:03:25 -0400

    Remove `goto` from router
    Signed-off-by: Fong-Wan Chau <work@fongwanchau.com>

--- main.go
+++ main.go
@@ -35,6 +35,82 @@ import (
 // HDX multiplexer
 type HDXMux struct{}
 
+func getBusinessData(domain string) int {
+	var (
+		isBusinessDataCache bool  = false
+		err                 error // Error handler
+	)
+
+	// Check whether data exist in business data cache
+	for key := range shared.BusinessData {
+		if domain == key {
+			isBusinessDataCache = true
+		}
+	}
+
+	if !isBusinessDataCache {
+		// No previous cache, initialize new cache
+		shared.BusinessData[domain] = &shared.BusinessInfo{}
+
+		// Get the business data
+		err = shared.DB.QueryRow("SELECT id, country, mainLanguage FROM business WHERE path = ? LIMIT 1", domain).Scan(
+			&shared.BusinessData[domain].Id,
+			&shared.BusinessData[domain].Country,
+			&shared.BusinessData[domain].Language)
+
+		switch {
+		// In case that the business is not registered
+		case err == sql.ErrNoRows:
+			// Return 403 invalid_client
+			return http.StatusNotFound
+		// In case any other error
+		case err != nil:
+			log.Fatal(err)
+			return http.StatusInternalServerError
+		}
+	} else {
+		// In case that the business is not registered
+		if 0 == shared.BusinessData[domain].Id {
+			// Return 403 invalid_client
+			return http.StatusNotFound
+		}
+	}
+
+	return http.StatusOK
+}
+
+func checkAuthorization(ah string, currentConn *shared.ConnInfo) int {
+	var tokenMsg shared.UserInfo // Token data
+
+	// Get the token on the "Authorization" header if exist
+	if "" != ah {
+		// Check whether the "Authorization" header has a valid format
+		// The header should be a HDX token
+		if 4 < len(ah) && "HDX " == strings.ToUpper(ah[0:4]) {
+			// Try to get the message and cert the token
+			if raw := token.Get(currentConn.Fingerprint, currentConn.Domain, ah[4:], shared.SECRET_KEY); "" != raw {
+				// Decifrate the JSON message and check whether the token expired
+				if err := json.Unmarshal([]byte(raw), &tokenMsg); err != nil || currentConn.RequestTime > tokenMsg.Exp {
+					// Token expired, return 401 unauthorized
+					return http.StatusUnauthorized
+				}
+
+				// Everything is ok
+				currentConn.UserInfo = &tokenMsg
+				return http.StatusOK
+			} else {
+				// 'Authorization' header invalid, return 401 unauthorized
+				return http.StatusUnauthorized
+			}
+		} else {
+			// 'Authorization' header invalid, return 401 unauthorized
+			return http.StatusUnauthorized
+		}
+	}
+
+	return http.StatusOK
+}
+
 // HDX default router
 func (m *HDXMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var (
@@ -54,112 +130,46 @@ func (m *HDXMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		path string = strings.Trim(strings.TrimPrefix(r.RequestURI, PATH_PREFIX), "/")
 		page string = strings.SplitN(path, "/", 2)[0]
 
-		// Token data
-		tokenMsg shared.UserInfo
-
-		// Error handler
-		err error
-
 		// Client fingerprint encripter
 		fingerprintCrypto = sha256.New()
 	)
-
-	// Check whether data exist in business data cache
-	var isBusinessDataCache bool = false
-	for key := range shared.BusinessData {
-		if currentConn.Domain == key {
-			isBusinessDataCache = true
-		}
-	}
-
-	if !isBusinessDataCache {
-		// No previous cache, initialize new cache
-		shared.BusinessData[currentConn.Domain] = &shared.BusinessInfo{}
-
-		// Get the business data
-		err = shared.DB.QueryRow("SELECT id, country, mainLanguage FROM business WHERE path = ? LIMIT 1", currentConn.Domain).Scan(
-			&shared.BusinessData[currentConn.Domain].Id,
-			&shared.BusinessData[currentConn.Domain].Country,
-			&shared.BusinessData[currentConn.Domain].Language)
-		switch {
-		// In case that the business is not registered
-		case err == sql.ErrNoRows:
-			// Return 403 invalid_client
-			currentConn.Response.StatusCode = http.StatusNotFound
-			goto response
-		// In case any other error
-		case err != nil:
-			log.Fatal(err)
-		}
-	} else {
-		// In case that the business is not registered
-		if 0 == shared.BusinessData[currentConn.Domain].Id {
-			// Return 403 invalid_client
-			currentConn.Response.StatusCode = http.StatusNotFound
-			goto response
-		}
-	}
 
 	// Generate client fingerprint using IP address, user agent and domain
 	// The value is base64 encoded
 	io.WriteString(fingerprintCrypto, r.Header.Get("RemoteAddr")+r.UserAgent()+currentConn.Domain)
 	currentConn.Fingerprint = base64.RawURLEncoding.EncodeToString(fingerprintCrypto.Sum(nil))
 
-	// Get the token on the "Authorization" header if exist
-	if ah := r.Header.Get("Authorization"); "" != ah {
-		// Check whether the "Authorization" header has a valid format
-		// The header should be a HDX token
-		if 4 < len(ah) && "HDX " == strings.ToUpper(ah[0:4]) {
-			// Try to get the message and cert the token
-			if raw := token.Get(currentConn.Fingerprint, currentConn.Domain, ah[4:], shared.SECRET_KEY); "" != raw {
-				// Decifrate the JSON message and check whether the token expired
-				if err = json.Unmarshal([]byte(raw), &tokenMsg); err != nil || currentConn.RequestTime > tokenMsg.Exp {
-					// Token expired, return 401 unauthorized
-					currentConn.Response.StatusCode = http.StatusUnauthorized
-					goto response
+	// Get the business data and check whether business data is OK
+	currentConn.Response.StatusCode = getBusinessData(currentConn.Domain)
+	if http.StatusOK == currentConn.Response.StatusCode {
+		// Check "Authorization" header
+		currentConn.Response.StatusCode = checkAuthorization(r.Header.Get("Authorization"), &currentConn)
+		if http.StatusOK == currentConn.Response.StatusCode {
+			// Check whether page exist if there authorized
+			if modules.IsExist(page) {
+				// Pass business info to current connection data
+				currentConn.BusinessInfo = shared.BusinessData[currentConn.Domain]
+
+				switch r.Method {
+				case http.MethodGet:
+					modules.Get(page, &currentConn)
+				case http.MethodPost:
+					modules.Post(page, &currentConn)
+				case http.MethodPut:
+					modules.Put(page, &currentConn)
+				case http.MethodPatch:
+					modules.Patch(page, &currentConn)
+				case http.MethodDelete:
+					modules.Delete(page, &currentConn)
+				default:
+					currentConn.Response.StatusCode = http.StatusMethodNotAllowed
 				}
-
-				currentConn.UserInfo = &tokenMsg
 			} else {
-				// 'Authorization' header invalid, return 401 unauthorized
-				currentConn.Response.StatusCode = http.StatusUnauthorized
-				goto response
+				currentConn.Response.StatusCode = http.StatusNotFound
 			}
-		} else {
-			// 'Authorization' header invalid, return 401 unauthorized
-			currentConn.Response.StatusCode = http.StatusUnauthorized
-			goto response
 		}
 	}
 
-	// Go here if 'Authorization' header is valid or not exist
-
-	// Check whether page exist
-	if modules.IsExist(page) {
-		// Pass business info to current connection data
-		currentConn.BusinessInfo = shared.BusinessData[currentConn.Domain]
-
-		switch r.Method {
-		case http.MethodGet:
-			modules.Get(page, &currentConn)
-		case http.MethodPost:
-			modules.Post(page, &currentConn)
-		case http.MethodPut:
-			modules.Put(page, &currentConn)
-		case http.MethodPatch:
-			modules.Patch(page, &currentConn)
-		case http.MethodDelete:
-			modules.Delete(page, &currentConn)
-		default:
-			currentConn.Response.StatusCode = http.StatusMethodNotAllowed
-			goto response
-		}
-	} else {
-		currentConn.Response.StatusCode = http.StatusNotFound
-		goto response
-	}
-
-response:
 	// Set headers to match JSON response and avoid being cached by browser
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.Header().Set("Cache-Control", "no-cache, no-store")
